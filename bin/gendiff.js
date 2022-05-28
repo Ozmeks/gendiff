da6ad725a4f37413e58f8bcfea22864caad793ee
ozmeks <ozmeks@yandex.ru>
2022-05-28 18:15:04 +0400

    refactore some calls

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,13 +5,11 @@ import gendiff from '../src/formatters/index.js';
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.9')
+  .version('1.0')
   .option('-f, --format [type]', 'output format: stylish, plain, json', 'stylish')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2, program.format));
+  })
   .parse(process.argv);
-
-const { args } = program;
-const [file1, file2] = args;
-const { format } = program.opts();
-console.log(gendiff(file1, file2, format));
