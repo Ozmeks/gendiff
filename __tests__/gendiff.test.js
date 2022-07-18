6d12a45791419f6685f7a4e98b4738f18e82dee4
ozmeks <ozmeks@yandex.ru>
2022-07-18 13:28:14 +0500

    refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -13,34 +13,12 @@ const fileJson2 = getFixturePath('file2.json');
 const fileYaml1 = getFixturePath('file1.yml');
 const fileYaml2 = getFixturePath('file2.yml');
 
-const expectedPlainResult = readFile('expected_plain.md').toString();
-const plainResultJson = gendiff(fileJson1, fileJson2, 'plain');
-test('Compare JSON files and check plain format', () => {
-  expect(plainResultJson).toBe(expectedPlainResult);
-});
-const plainResultYaml = gendiff(fileYaml1, fileYaml2, 'plain');
-test('Compare YAML files and check plain format', () => {
-  expect(plainResultYaml).toBe(expectedPlainResult);
-});
+const formats = ['plain', 'stylish', 'json'];
 
-const expectedStylishResult = readFile('expected_stylish.md').toString();
-const stylishResultJson = gendiff(fileJson1, fileJson2, 'stylish');
-test('Compare JSON files and check stylsih format', () => {
-  expect(stylishResultJson).toBe(expectedStylishResult);
-});
-
-const stylishResultYaml = gendiff(fileYaml1, fileYaml2, 'stylish');
-test('Compare YAML files and check stylsih format', () => {
-  expect(stylishResultYaml).toBe(expectedStylishResult);
-});
-
-const expectedJsonStyleResult = readFile('expected_json.json').toString();
-const jsonStyleResultJson = gendiff(fileJson1, fileJson2, 'json');
-test('Compare JSON files and check json format', () => {
-  expect(jsonStyleResultJson).toBe(expectedJsonStyleResult);
-});
-
-const jsonStyleResultYaml = gendiff(fileYaml1, fileYaml2, 'json');
-test('Compare YAML files and check json format', () => {
-  expect(jsonStyleResultYaml).toBe(expectedJsonStyleResult);
+test.each(formats)('Test %s format', (format) => {
+  const expectedResult = readFile(`expected_${format}.md`).toString();
+  const receivedJson = gendiff(fileJson1, fileJson2, format);
+  expect(receivedJson).toEqual(expectedResult);
+  const receivedYaml = gendiff(fileYaml1, fileYaml2, format);
+  expect(receivedYaml).toEqual(expectedResult);
 });
